c530fc71242d105197b7a9f662ff6d4f26f18c13
T1maaaa <156171264+T1maaaa@users.noreply.github.com>
2024-03-05 19:18:14 +0000

    p

---
+++ main.py
@@ -0,0 +1,15 @@
+for w in 'hello':
+    print('character',w)
+S='Odesssssa'
+new_s=''
+prev_w=''
+for w in s:
+    if w==prev_w:
+        continue
+    else:
+        new_s=new_s+w
+        prev_w=w
+        if w!=prev_w
+        new_s+=w
+        prev_w=w
+
